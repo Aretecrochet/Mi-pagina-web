c683b5ad4f94ea9513a9bd415db534fa99b1aab8
gabriel.q <gabriel.q@regionglobal.com>
2024-07-08 09:30:07 -0300

    cards con carrousel

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+
+"use strict"
+
+console.log("load");
+
+$(document).ready(function() {
+    $('#view-product-details').click(function() {
+        window.location.href = 'pages/product_details.html';
+    });
+});
